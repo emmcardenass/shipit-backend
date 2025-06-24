9f8ee814f99ba48cba759a8bcbde53ba13dbe2d0
Emmanuel Cardenas <emmanuelcardenas@MacBook-Air-de-Emmanuel.local>
2025-06-24 12:35:05 -0600

    Fix: Corrección en server.js

--- server.js
+++ server.js
@@ -29,7 +29,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // 🛡️ Middlewares
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://shipit-frontend-q97x.onrender.com"], // Ajusta la URL final de tu frontend si cambia
+  credentials: true
+}));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
