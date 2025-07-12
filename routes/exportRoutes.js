66db66125182d93ff3a260fa36e32998817adb9e
Emmanuel Cardenas <emmanuelcardenas@MacBook-Air-de-Emmanuel.local>
2025-07-12 12:18:31 -0600

    🐛 Fix: removed unused isAdmin import in exportRoutes.js

--- routes/exportRoutes.js
+++ routes/exportRoutes.js
@@ -1,14 +1,13 @@
 import express from "express";
 import { exportarRecolecciones, exportarEntregas } from "../controllers/exportPedidosController.js";
 import authMiddleware from "../middleware/authMiddleware.js";
-import isAdmin from "../middlewares/isAdmin.js";
 
 const router = express.Router();
 
 // Ruta para descargar recolecciones
-router.get("/recolecciones", authMiddleware, isAdmin, exportarRecolecciones);
+router.get("/recolecciones", authMiddleware, exportarRecolecciones);
 
 // Ruta para descargar entregas
-router.get("/entregas", authMiddleware, isAdmin, exportarEntregas);
+router.get("/entregas", authMiddleware, exportarEntregas);
 
 export default router;
